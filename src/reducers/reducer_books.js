2086e2d0a82c07fb9860028b181426e912d87073
Luong Vo <vo.tran.thanh.luong@gmail.com>
2017-05-04 14:28:34 +0700

    Following the original tutorial

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+ export default function() {
+  return [
+    {title: 'Book a', pages : 121},
+    {title: 'Book b', pages : 124234},
+    {title: 'Book c', pages : 343},
+    {title: 'Book d', pages : 2344},
+    {title: 'Book e', pages : 65},
+    {title: 'Book f', pages : 994}
+  ]
+}
